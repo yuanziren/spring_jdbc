369c5799153d81d5592cc1382ee95699ba1e6081
yuanziren <1245765114@qq.com>
2019-08-04 16:36:54 +0800

    Spring事务管理

--- src/main/java/com/yuanziren/service/AccountService.java
+++ src/main/java/com/yuanziren/service/AccountService.java
@@ -3,6 +3,8 @@ package com.yuanziren.service;
 import com.yuanziren.dao.AccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AccountService {
@@ -16,7 +18,14 @@ public class AccountService {
      * @param inId
      * @param money
      */
+    @Transactional(propagation = Propagation.REQUIRED)
     public void zhuanzhang(int outId, int inId, double money){
+        accountDao.outMoney(outId, money);
+        int i = 1/0;
+        accountDao.inMoney(inId, money);
+    }
+
+    public void updateAccountMoney(int outId, int inId, double money){
         accountDao.outMoney(outId, money);
         int i = 1/0;
         accountDao.inMoney(inId, money);
